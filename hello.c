964d8a6a8780c85cd1678f6858e28f2880e55576
mirimsong05 <mirimsong05@gmail.com>
2026-09-14 21:08:24 +0900

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+int main(void)
 {
-    printf("Hello, Sookmyung Electronics!\n");
+    int a, b;
+
+    a = 10;
+    b = 20;
+
+    printf("%d + %d = %d\n", a, b, a + b);
+
     return 0;
 }
